6c2c61d41d582bd5a4c247c903ea1e18de7af46d
vzariptv <153348494+VikZarip@users.noreply.github.com>
2025-08-12 00:09:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -92,9 +92,11 @@ def create_app():
 
     return app
 
+# Создаем экземпляр приложения для Render/Gunicorn
+app = create_app()
+
 # Локальный запуск (python app.py)
 if __name__ == "__main__":
-    app = create_app()
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8083)),
